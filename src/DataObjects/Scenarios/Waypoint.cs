c3471ebc7d952a8656ac1a9387aa59d9db1ba6ef
René Weiser <rene@encoding.group>
2021-07-02 12:47:38 +0200

    Forcing deserialization with JsonProperty

--- src/DataObjects/Scenarios/Waypoint.cs
+++ src/DataObjects/Scenarios/Waypoint.cs
@@ -1,8 +1,12 @@
-﻿namespace Vreval.Platform.DataObjects.Scenarios
+﻿using Newtonsoft.Json;
+
+namespace Vreval.Platform.DataObjects.Scenarios
 {
     public class Waypoint
     {
+        [JsonProperty("checkpoint_id")]
         public int CheckpointId;
+        [JsonProperty("form_id")]
         public int FormId;
         public string Name;
     }
