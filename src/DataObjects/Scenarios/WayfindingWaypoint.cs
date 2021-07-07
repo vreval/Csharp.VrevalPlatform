408d03aa998fe5166c57ddb506aa501f1d47abfe
René Weiser <rene@encoding.group>
2021-07-07 14:35:52 +0200

    Added jsonproperty to dataobject

--- src/DataObjects/Scenarios/WayfindingWaypoint.cs
+++ src/DataObjects/Scenarios/WayfindingWaypoint.cs
@@ -1,7 +1,10 @@
-﻿namespace Vreval.Platform.DataObjects.Scenarios
+﻿using Newtonsoft.Json;
+
+namespace Vreval.Platform.DataObjects.Scenarios
 {
     public class WayfindingWaypoint : Waypoint
     {
+        [JsonProperty("is_optional")]
         public bool IsOptional;
     }
 }
